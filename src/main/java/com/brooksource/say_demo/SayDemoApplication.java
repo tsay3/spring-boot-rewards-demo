d336414a1640662fd0e9d27d8c05bad58d500ee6
Thomas Say <tsay3@uic.edu>
2025-11-19 15:46:44 -0600

    Initial commit

---
+++ src/main/java/com/brooksource/say_demo/SayDemoApplication.java
@@ -0,0 +1,15 @@
+package com.brooksource.say_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SayDemoApplication.class, args);
+
+	}
+
+}
