e8d3aa94e9bfd477856075def8b3409c304dab9c
Thomas Say <tsay3@uic.edu>
2025-11-20 21:06:34 -0600

    Updated deprecated Date format

--- src/main/java/com/brooksource/saydemo/util/DateStorage.java
+++ src/main/java/com/brooksource/saydemo/util/DateStorage.java
@@ -3,9 +3,10 @@ package com.brooksource.saydemo.util;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateStorage {
     public static int timestampToMonth(Timestamp t) {
@@ -13,15 +14,16 @@ public class DateStorage {
     }
     public static int timestampToMonth(Instant i) {
         ZoneId zone = ZoneId.of("America/New_York");
-        int yearValue = i.atZone(zone).getYear() - 1900;
+        int yearValue = i.atZone(zone).getYear();
         int monthValue = i.atZone(zone).getMonth().getValue() - 1;
         return monthValue + 12 * yearValue;
     }
     public static String monthValueToString(int monthValue) {
         int year = monthValue / 12;
         int month = monthValue % 12;
-        LocalDate date = LocalDate.of(year, month, 1);
-        return new SimpleDateFormat("MMM yyyy").format(date);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM yyyy");
+        GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+        return cal.toZonedDateTime().format(dtf).toString();
     }
 
     public static String generationMessage() {
