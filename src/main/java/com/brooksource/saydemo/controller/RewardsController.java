99576008b8a689cdb121b8afef621b7ed0b25fcd
Thomas Say <tsay3@uic.edu>
2025-11-20 18:51:10 -0600

    Setting up for all customer points

--- src/main/java/com/brooksource/saydemo/controller/RewardsController.java
+++ src/main/java/com/brooksource/saydemo/controller/RewardsController.java
@@ -13,37 +13,19 @@ import java.time.Instant;
 @RestController
 public class RewardsController {
 
-    @Autowired
-    CustomerService customerService;
+//    @Autowired
+//    CustomerService customerService;
     @Autowired
     TransactionService transactionService;
 
+    @GetMapping("/points")
+    public String getAllPoints() {
+        return transactionService.getPointsResponseForAllUsers();
+    }
+
     @GetMapping("/points/{id}")
     public String getPointsById(@PathVariable Long id) {
-        StringBuilder returnHTML = new StringBuilder("<h1>Customer ");
-        returnHTML.append(customerService.getCustomerName(id));
-        returnHTML.append("</h1>");
-
-        int totalPoints = 0;
-        int currentMonth = DateStorage.timestampToMonth(Instant.now());
-
-        for (int i = currentMonth - 2; i <= currentMonth; i++) {
-            int monthsPoints = transactionService.getPointsForMonth(id, i);
-            returnHTML.append("<p>");
-            returnHTML.append(DateStorage.monthValueToString(i));
-            returnHTML.append(": ");
-            returnHTML.append(monthsPoints);
-            returnHTML.append(" points</p>");
-            totalPoints += monthsPoints;
-        }
-        returnHTML.append("<p><b>Total: ");
-        returnHTML.append(totalPoints);
-        returnHTML.append(" points</b></p>");
-        returnHTML.append("<i>Generated on ");
-        returnHTML.append(DateStorage.monthValueToString(
-                DateStorage.timestampToMonth(Instant.now())));
-        returnHTML.append("</i>");
-
-        return returnHTML.toString();
+        if (id == null) return getAllPoints();
+        return transactionService.getPointsResponseForUser(id);
     }
 }
--- src/main/java/com/brooksource/saydemo/service/TransactionService.java
+++ src/main/java/com/brooksource/saydemo/service/TransactionService.java
@@ -1,19 +1,60 @@
 package com.brooksource.saydemo.service;
 
 import com.brooksource.saydemo.repository.TransactionRepository;
+import com.brooksource.saydemo.util.DateStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.brooksource.saydemo.model.Transaction;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.List;
 
 @Service
 public class TransactionService {
+
+    @Autowired
+    CustomerService customerService;
     @Autowired
     TransactionRepository repo;
 
-    public int getPointsForMonth(Long id, int month) {
+    public String getPointsResponseForAllUsers() {
+        StringBuilder returnHTML = new StringBuilder("<h1>All Customers</h1>");
+        returnHTML.append("TO DO: add an endpoint to show ALL customers' points");
+
+        int currentMonth = DateStorage.timestampToMonth(Instant.now());
+        return returnHTML.toString();
+    }
+
+    public String getPointsResponseForUser(Long id) {
+        StringBuilder returnHTML = new StringBuilder("<h1>Customer ");
+        returnHTML.append(customerService.getCustomerName(id));
+        returnHTML.append("</h1>");
+
+        int totalPoints = 0;
+        int currentMonth = DateStorage.timestampToMonth(Instant.now());
+
+        for (int i = currentMonth - 2; i <= currentMonth; i++) {
+            int monthsPoints = getPointsForUserAndMonth(id, i);
+            returnHTML.append("<p>");
+            returnHTML.append(DateStorage.monthValueToString(i));
+            returnHTML.append(": ");
+            returnHTML.append(monthsPoints);
+            returnHTML.append(" points</p>");
+            totalPoints += monthsPoints;
+        }
+        returnHTML.append("<p><b>Total: ");
+        returnHTML.append(totalPoints);
+        returnHTML.append(" points</b></p>");
+        returnHTML.append("<i>Generated on ");
+        returnHTML.append(DateStorage.monthValueToString(
+                DateStorage.timestampToMonth(Instant.now())));
+        returnHTML.append("</i>");
+
+        return returnHTML.toString();
+    }
+
+    private int getPointsForUserAndMonth(Long id, int month) {
         int total = 0;
         List<Transaction> list = repo.findByMonthValue(month);
         for (Transaction oneTransaction : list) {
